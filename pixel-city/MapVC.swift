634bc7e1e0bffb5613ca0f58138fddcfa7500dc1
MattOsak <Matt@mxwshq9sdc2.na.intranet.msd>
2018-01-15 14:33:25 -0500

    first commit

---
+++ pixel-city/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pixel-city
+//
+//  Created by Matt Osak on 2018-01-15.
+//  Copyright © 2018 Matt Osak. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
