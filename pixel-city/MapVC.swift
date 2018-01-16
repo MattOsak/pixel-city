435c598ca86676a9e1884212af18177c372965a2
MattOsak <Matt@Matts-iMac.local>
2018-01-16 18:12:40 -0500

    commit

--- pixel-city/MapVC.swift
+++ pixel-city/MapVC.swift
@@ -7,17 +7,57 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 import Alamofire
+import AlamofireImage
+
 
 
 class MapVC: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var pullUpViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pullUpView: UIView!
+    let regionRadius: Double = 1000
+    
 
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
+        
     }
 
-
+    @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+    }
 
 }
+extension MapVC: MKMapViewDelegate {
 
+    func centerMapOnUserLocation () {
+        guard let corrdinate = locationManager.location?.coordinate else { return }
+        let corrdinateRegion = MKCoordinateRegionMakeWithDistance(corrdinate, regionRadius * 2.0 , regionRadius * 2.0)
+        mapView.setRegion(corrdinateRegion, animated: true)
+        
+    }
+    
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+}
